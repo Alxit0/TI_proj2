d29325f6918c83ba4c0b3f31d83cc40614cb57ed
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-11-30 14:16:02 +0000

    ??

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import move_to_front as mtf
 import btw
 
 
@@ -50,24 +51,28 @@ def diferenca(letras):
 
 
 values = ["bible.txt", "finance.csv", "jquery-3.6.0.js", "random.txt"]
-if __name__ == '__main__':
-    """i = values[-1]
-    a = get_info("dataset/"+i)
-    for i in (t := ''.join(a)):
-        if i.isalpha() or i == ' ':
-            print(end=i)
-    print("")
 
-    for i in btw.bwt(t):
-        if i.isalpha() or i == ' ':
-            print(end=i)
-    print("")
-"""
+
+def main1():
+    """i = values[-1]
+        a = get_info("dataset/"+i)
+        for i in (t := ''.join(a)):
+            if i.isalpha() or i == ' ':
+                print(end=i)
+        print("")
+
+        for i in btw.bwt(t):
+            if i.isalpha() or i == ' ':
+                print(end=i)
+        print("")"""
     i = values[3]
     a = get_info("dataset/" + i)
     print(i, entropia(a))
     b, cnt = np.unique(a, return_counts=True, axis=0)  # achar as contagens
     histograma(b, cnt, i, organizacao=True)
+def main():
+    print(mtf.encode("abacadda"))
 
 
-
+if __name__ == '__main__':
+    main()
