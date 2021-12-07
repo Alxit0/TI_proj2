beb0e80795fd2e0c2bd448176f070028de2acfa5
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-12-07 01:44:20 +0000

    decod

--- codificacoes/Huffman.py
+++ codificacoes/Huffman.py
@@ -27,46 +27,91 @@ if True:
             return "leaf"
 
 
-def find_paths(node: Node, path="", d=None):
-    if d is None:
-        d = {}
-
-    if node.key is not None:
-        d[node.key] = path
-        print(node.key, path)
-        return
-    find_paths(node.left, path+'0', d)
-    find_paths(node.right, path+'1', d)
-    return d
-
-
-def generate_tree(string: str):
-    valores = np.asarray([*string])
-    d = {i: j for i, j in zip(*np.unique(valores, return_counts=True, axis=0))}  # achar as contagens
-    temp: list[Node] = []
-    for i in sorted(d.keys(), key=d.__getitem__):
-        temp += Leaf(i, d[i]),
-
-    while len(temp) != 1:
-        a = temp.pop()
-        b = temp.pop()
-        temp += Node(a.value + b.value, a, b),
-        temp.sort(key=lambda x: x.value, reverse=True)
-
-    print(*map(str, temp))
-    return temp[0]
-
-
 def encode(s: str):
+    def find_paths(node: Node, path="", d=None):
+        if d is None:
+            d = {}
+
+        if node.key is not None:
+            d[node.key] = path
+            return
+        find_paths(node.left, path + '0', d)
+        find_paths(node.right, path + '1', d)
+        return d
+
+    def generate_tree(string: str):
+        valores = np.asarray([*string])
+        d = {k: j for k, j in zip(*np.unique(valores, return_counts=True, axis=0))}  # achar as contagens
+        temp: list[Node] = []
+        for i in sorted(d.keys(), key=d.__getitem__):
+            temp += Leaf(i, d[i]),
+
+        while len(temp) != 1:
+            a = temp.pop()
+            b = temp.pop()
+            temp += Node(a.value + b.value, a, b),
+            temp.sort(key=lambda x: x.value, reverse=True)
+        return temp[0]
+
+    def convert(seq):
+        a = ""
+        for i in range(0, len(seq), 8):
+            a += chr(int(seq[i:i + 8], 2))
+        return a
+
     arv = generate_tree(s)
     tabela = find_paths(arv)
     nova_s = ""
     for i in s:
         nova_s += tabela[i]
-    return arv, tabela, nova_s
+    return arv, convert(nova_s)
+    # return arv, nova_s
+
+
+def decode(s: str, arv: Node):
+    def translate(seq):
+        b = ""
+        for j in seq:
+            b += bin(ord(j))[2:].zfill(8)
+        return b
+
+    s = translate(s)
+    pointer = arv
+    resp = ''
+    for i in s:
+        if i == '0':
+            pointer = pointer.left
+        elif i == '1':
+            pointer = pointer.right
+
+        if pointer.key is not None:
+            resp += pointer.key
+            pointer = arv
+    return resp
+
+
+def main():
+    arvore, s_encoded = encode("Burrows–Wheeler transform => make easier to compress (rearanges in order to make "
+                               "more repeated leters together)")
+    print(arvore)
+    print(s_encoded)
+
+    print(decode(s_encoded, arvore))
+
+
+def main1():
+    temp = "01011101110"
+    print(temp)
+    a = ""
+    for i in range(0, len(temp), 8):
+        a += chr(int(temp[i:i+8], 2))
+    print(a)
+
+    b = ""
+    for i in a:
+        b += bin(ord(i))[2:]
+    print(b)
 
 
 if __name__ == '__main__':
-    a, t, s_encoded = encode("banana")
-    print(s_encoded)
-
+    main()
