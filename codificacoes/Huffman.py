1579a81e86a17b5bf742b82638f5d5a08b70ba60
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-12-07 00:51:39 +0000

    huffman codec basic

--- codificacoes/Huffman.py
+++ codificacoes/Huffman.py
@@ -1,42 +1,72 @@
 import numpy as np
+# classes
+if True:
+    class Node:
+        def __init__(self, value=0, left=None, right=None, key=None):
+            self.left = left
+            self.right = right
+            self.value = value
+            self.key = key
+
+        def __str__(self):
+            return f"){self.left},{self.right}("
+
+        def get_type(self):
+            return "normal"
 
 
-class leaf:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
+    class Leaf(Node):
+        def __init__(self, key, value):
+            super().__init__(value)
+            self.key = key
 
-    def __str__(self):
-        return f"<{self.key},{self.value}>"
+        def __str__(self):
+            return f"<{self.key},{self.value}>"
 
-
-class tree_node:
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
-        self.value = left.value + right.value
-
-    def __str__(self):
-        return f"){self.left} {self.right}("
-
-    def get_paths(self):pass
+        def get_type(self):
+            return "leaf"
 
 
-def encode(string: str):
+def find_paths(node: Node, path="", d=None):
+    if d is None:
+        d = {}
+
+    if node.key is not None:
+        d[node.key] = path
+        print(node.key, path)
+        return
+    find_paths(node.left, path+'0', d)
+    find_paths(node.right, path+'1', d)
+    return d
+
+
+def generate_tree(string: str):
     valores = np.asarray([*string])
     d = {i: j for i, j in zip(*np.unique(valores, return_counts=True, axis=0))}  # achar as contagens
-    temp = []
+    temp: list[Node] = []
     for i in sorted(d.keys(), key=d.__getitem__):
-        temp += leaf(i, d[i]),
+        temp += Leaf(i, d[i]),
+
     while len(temp) != 1:
         a = temp.pop()
         b = temp.pop()
-        temp += tree_node(a, b),
+        temp += Node(a.value + b.value, a, b),
         temp.sort(key=lambda x: x.value, reverse=True)
 
     print(*map(str, temp))
+    return temp[0]
+
+
+def encode(s: str):
+    arv = generate_tree(s)
+    tabela = find_paths(arv)
+    nova_s = ""
+    for i in s:
+        nova_s += tabela[i]
+    return arv, tabela, nova_s
 
 
 if __name__ == '__main__':
-    encode("banana")
-    print(type(leaf('a', 0)))
+    a, t, s_encoded = encode("banana")
+    print(s_encoded)
+
