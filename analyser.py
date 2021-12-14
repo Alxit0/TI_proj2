e51ddd8938c85dd3bf71dec9958dffda03774ad8
Alexandre Regalado <alexandreregalado@hotmail.com>
2021-12-14 02:53:32 +0000

    metodos para codificar sequencias binarias

--- analyser.py
+++ analyser.py
@@ -1,4 +1,19 @@
 def is_equal(name1: str, name2: str):
-    file1 = open(name1, "r")
-    file2 = open(name2, "r")
-    return file1.readlines() == file2.readlines()
+    file1 = open(name1, "r").readlines()
+    file2 = open(name2, "r").readlines()
+    temp = file1 == file2
+
+    #if not temp: see_dif(file1, file2)
+
+    return temp
+
+
+def em_numeros(*seq):
+    for i in zip(*seq):
+        print(*map(ord, i))
+
+
+def see_dif(a, b):
+    for i, j in zip(a, b):
+        if i != j:
+            print(i, j)
